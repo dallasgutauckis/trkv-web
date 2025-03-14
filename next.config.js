cd8ddd0a05f49a6194b9741ef5f06d6e8a3fa000
Dallas Gutauckis <dallasg@gmail.com>
2025-03-14 00:45:03 -0400

    Add Twitch CDN domains to Next.js image configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    images: {
+        domains: [
+            'static-cdn.jtvnw.net',  // Twitch profile images
+            'cdn.jtvnw.net',         // Alternate Twitch CDN domain
+            'jtvnw.net',             // Base Twitch CDN domain
+        ],
+    },
 };
 
 module.exports = nextConfig; 
